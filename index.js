e56db9d99ed120b485ff6663e081766da3d7cf12
MubarakOke <okeolamubarak18@gmail.com>
2023-05-08 12:48:43 +0100

    created public key and secret key

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const {Connection,
         LAMPORTS_FOR_SOL} = require('@solana/web3.js')
 
 const wallet= new Keypair()
+const publicKey= new PublicKey(wallet._keypair.publicKey)
+const secretKey= wallet._keypair.secretKey
+console.log("pubulic key", publicKey)
+console.log("secret key", secretKey)
