ba714964be7c90e473dc7c330514e532da23ab5e
MubarakOke <okeolamubarak18@gmail.com>
2023-05-08 13:00:54 +0100

    created get wallet functions

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const {Connection, 
         PublicKey, 
-        ClusterApiUrl, 
         Keypair, 
-        LAMPORTS_FOR_SOL} = require('@solana/web3.js')
+        LAMPORTS_FOR_SOL,
+        clusterApiUrl} = require('@solana/web3.js')
 
 const wallet= new Keypair()
 const publicKey= new PublicKey(wallet._keypair.publicKey)
 const secretKey= wallet._keypair.secretKey
+
+const getWalletBalance= async ()=>{
+    try{
+        const connection= new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance= await connection.getBalance(publicKey)
+        console.log("wallet balance is :",walletBalance)
+    }
+    catch (err){
+
+    }
+}
+
+const main= (async()=>{
+    await getWalletBalance()
+})()
+
